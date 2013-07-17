70bc23740299be86c660216171a87e89046ac72d
Joseph Bergdoll <j@varsity.is>
2013-07-17 19:52:33 -0400

    Homepage, Cart, Product Index, Product, Mission

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,26 @@
 $(document).ready(function(){
 
 	$("div.desc").hide();
+	$("section.cart-disp").hide();
 	
 	$("a.product").hover(function(){
 		$(this).children('div.desc').fadeToggle(150);
 	});
+	
+	$("span.cart a").click(function(){
+		$("section.cart-disp").fadeToggle(200);
+		$("span.cart a").toggleClass("cartclicked");
+		$("span.cart figure").toggleClass("cartclicked")
+	});
+	
+	$("a.closebtn").click(function() {
+		$(this).closest("section.cart-disp").fadeOut(350);
+		$("span.cart a").removeClass("cartclicked");
+		$("span.cart figure").removeClass("cartclicked");
+	});
+
+
+	$('.mySelectBoxClass').customSelect({customClass:'colorsize'});
+
 	
 });

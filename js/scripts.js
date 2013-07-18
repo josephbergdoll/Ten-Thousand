1256f0cb98e43240e0f48c973c16f4e82e369d9f
Joseph Bergdoll <j@varsity.is>
2013-07-18 17:16:07 -0400

    All heading and type styles fixed, link structure fixed, empty cart added

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,27 @@
 $(document).ready(function(){
 
 	$("div.desc").hide();
-	$("section.cart-disp").hide();
+	$("section.cart-disp-full").hide();
+	$("section.cart-disp-empty").hide();
 	$("div.closebtn").hide();
 	
 	$("a.product").hover(function(){
 		$(this).children('div.desc').fadeToggle(150);
 	});
 	
-	$("span.cart a").click(function(){
-		$("section.cart-disp").fadeToggle(200);
+	$("body.full span.cart a").click(function(){
+		$("section.cart-disp-full").fadeToggle(200);
 		$("span.cart a").toggleClass("cartclicked");
 		$("span.cart figure").toggleClass("cartclicked");
 		$("div.closebtn").fadeToggle(300);
 	});
 	
-
-
-
-	$('.mySelectBoxClass').customSelect({customClass:'colorsize'});
+	$("body.empty span.cart a").click(function(){
+		$("section.cart-disp-empty").fadeToggle(200);
+		$("span.cart a").toggleClass("cartclicked");
+		$("span.cart figure").toggleClass("cartclicked");
+		$("div.closebtn").fadeToggle(300);
+	});
 
 	
 });

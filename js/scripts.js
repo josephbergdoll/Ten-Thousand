c7aa5d076aaf2a60079990b35d511fb168d82fd9
Joseph Bergdoll <j@varsity.is>
2013-07-18 13:39:51 -0400

    Type weights fixed, cart X fixed

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 
 	$("div.desc").hide();
 	$("section.cart-disp").hide();
+	$("div.closebtn").hide();
 	
 	$("a.product").hover(function(){
 		$(this).children('div.desc').fadeToggle(150);
@@ -10,14 +11,11 @@ $(document).ready(function(){
 	$("span.cart a").click(function(){
 		$("section.cart-disp").fadeToggle(200);
 		$("span.cart a").toggleClass("cartclicked");
-		$("span.cart figure").toggleClass("cartclicked")
+		$("span.cart figure").toggleClass("cartclicked");
+		$("div.closebtn").fadeToggle(300);
 	});
 	
-	$("a.closebtn").click(function() {
-		$(this).closest("section.cart-disp").fadeOut(350);
-		$("span.cart a").removeClass("cartclicked");
-		$("span.cart figure").removeClass("cartclicked");
-	});
+
 
 
 	$('.mySelectBoxClass').customSelect({customClass:'colorsize'});

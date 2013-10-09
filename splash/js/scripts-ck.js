d2bb189dc88d3a9ee79a0e13b804e33d5f82f2d0
Joseph Bergdoll <j@varsity.is>
2013-10-09 15:37:33 -0400

    Splash Screens

---
+++ splash/js/scripts-ck.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".submitform").delay(500).queue(function(){$(this).addClass("submit-reveal")})});
---
+++ splash/js/scripts.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+  $(".submitform").delay(500).queue(function(){$(this).addClass('submit-reveal');});
+});

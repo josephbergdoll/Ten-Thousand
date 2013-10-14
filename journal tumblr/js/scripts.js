6bd6da01335659f2c2114e6b88828e4a006c4aa4
Joseph Bergdoll <j@varsity.is>
2013-10-14 16:36:13 -0400

    Journal tumblr Updates

--- journal tumblr/js/scripts.js
+++ journal tumblr/js/scripts.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
-$("body.empty span.cart a").click(function(){
+
+	$("span.cart").hover(function(e){
+      $(".counter").toggleClass("cart-circle-hovered");
+      $(".js-cart-counter").toggleClass("cart-circle-span-hovered");
+  });
+
+$("span.cart a").click(function(){
 		$("section.cart-disp-empty").fadeToggle(200);
 		$("span.cart a").toggleClass("cartclicked");
 		$("span.cart figure").toggleClass("cartclicked");
